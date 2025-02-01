6be1ef79226d782fdfd9be1c129713da11b9c3ee
Christian McKern <ctmckern0625@gmail.com>
2025-01-31 20:12:49 -0500

    More tidying up.

--- skill-calc/src/Base.java
+++ skill-calc/src/Base.java
@@ -31,38 +31,25 @@ public class Base {
 //            MAIN_MENU_PLAYER2_RATING_PROMPT, MAIN_MENU_CHANCE_TO_WIN, MAIN_MENU_CHANCE_TO_LOSE,
 //            MAIN_MENU_POSSIBLE_WINS_OR_LOSSES, MAIN_MENU_POSSIBLE_LOSSES};
 
-    int i;
-    private double rating;
-    private static double[] ratingArray;
-    private double expectation;
-    //Need to move kFactor array to calculator class.
-    private int[] kFactors = {16,24,32};
+
     //When formatting remember DecimalFormat.format returns a string you will need to cast back to a double
     private DecimalFormat defo = new DecimalFormat("#.##");
     //http://www.glicko.net/ratings/approx.pdf   use this for referring to skill ratings.
 
-    /*
-    To do
+    /* To do
     Work on setting up extensions to calculator for specific skill rating algorithms
     Develop more prompts for readability and communication
      */
 
     private void getRatingFromUserInput(double[] ratings, int index) {
         String input = in.nextLine();
-        rating = Double.parseDouble(input);
+        double rating = Double.parseDouble(input);
         ratings[index] = rating;
         System.out.println();
     }
 
-    //pointsToGain should probably be in Calculator
     //https://rpubs.com/riazakhan94/logstcdistbasics Some basic info on the logistic distribution
     //which is what the Elo rating system uses
-    private double pointsToGain(double[]ratings, int kFactorIndex, double expectedScore, double actualScore){
-        double a = ratings[0];
-        double b = ratings[1];
-        double results = actualScore - expectedScore;
-        return kFactors[kFactorIndex] * results;
-    }
 
     private void setRatingArray(double[] Ratings, int index, double rating){
         Ratings[index] = rating;
