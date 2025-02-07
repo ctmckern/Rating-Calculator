2cef2b677865dabe6bb7741d32b2e8d3cb567f23
Christian McKern <ctmckern0625@gmail.com>
2025-02-06 21:16:16 -0500

    More of the glicko system is done

--- skill-calc/src/Controller/GlickoCalculator.java
+++ skill-calc/src/Controller/GlickoCalculator.java
@@ -6,6 +6,7 @@ public class GlickoCalculator {
 
     }
 
+    //Note that the glicko system can accommodate arrays of matches
     private double base = 10.0;
     private double q = Math.log(base)/400;
     private double ratingDeviation = 350;
@@ -14,7 +15,13 @@ public class GlickoCalculator {
     private double g(){
         return 1/Math.sqrt( ( 1 + (3*q*q) * (ratingDeviation*ratingDeviation) ) / Math.PI );
     }
+    //To-do
+    //Flesh out the d^2 formula(p and q are ready to go)
+    //Get the r' and RD' methods worked out(not immediate since there is no persistence layer)
 
+
+    //if probability/expectation is p then 1 - expectation is q
+    //This leads d^2 to become q^2*g(RD)^2*p*q which is far more digestible than the full version of this
     public double expectation(double[] ratings){
         double a = ratings[0];
         double b = ratings[1];
@@ -23,5 +30,10 @@ public class GlickoCalculator {
         double scaledDifference = skillDifference * scalar;
         double denominator = 1 + Math.pow(10, scaledDifference);
         return 1/denominator;
+    }
+
+    public double expectationAgainst(double[] ratings){
+        double expectedValue = expectation(ratings);
+        return 1-expectedValue;
     }
 }
